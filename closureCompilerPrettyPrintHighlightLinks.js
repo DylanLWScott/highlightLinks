84032cda31e925bb05f0c4cec9c941a3b4f1ae32
Dylan Scott <drrscott@hotmail.com>
2015-03-09 23:24:41 -0500

    Update closureCompilerPrettyPrintHighlightLinks.js

--- closureCompilerPrettyPrintHighlightLinks.js
+++ closureCompilerPrettyPrintHighlightLinks.js
@@ -1,21 +1,22 @@
-for(var a = document.getElementsByTagName("a"), b = a.length, c = [], i = 0;i < b;i++) {
-  a[i].insertAdjacentHTML("afterbegin", '<span class="hl" style="background-color:yellow;font-size:18px">' + i + "</span>"), c[i] = a[i]
+for(var b = document.getElementsByTagName("a"), c = b.length, d = [], i = 0;i < c;i++) {
+  b[i].insertAdjacentHTML("afterbegin", '<span class="hl" style="background-color:yellow;font-size:18px">' + i + "</span>"), d[i] = b[i]
 }
-var d = [], g;
+var e = [], g;
 document.onkeydown = function(f) {
   if(27 == f.keyCode) {
-    d = [], c = [], Array.prototype.forEach.call(document.querySelectorAll("span.hl"), function(e) {
-      e.remove()
+    e = [], d = [], Array.prototype.forEach.call(document.querySelectorAll("span.hl"), function(a) {
+      a.remove()
     }), g && window.clearTimeout(g)
   }else {
-    d.push(f.keyCode);
+    e.push(f.keyCode);
     g && window.clearTimeout(g);
-    var h = d.map(function(e) {
-      return String.fromCharCode(e)
+    var h = e.map(function(a) {
+      return String.fromCharCode(a)
     });
     g = window.setTimeout(function() {
-      d = [];
-      c[parseInt(h.join(""), 10)].click()
+      e = [];
+      var a = d[parseInt(h.join(""), 10)];
+      a && a.click()
     }, 650)
   }
 };
